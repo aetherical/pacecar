fd89c517a15a56e0075d0b314596eebcc01a398e
Matt Williams <matt@aetherical.com>
2011-03-10 16:22:36 -0500

    modified:   lib/pacecar/associations.rb Added the ability to have recently updated records, just like has_recent_records association.

--- lib/pacecar/associations.rb
+++ lib/pacecar/associations.rb
@@ -28,12 +28,39 @@ module Pacecar
         end
       end
 
+      def has_updated_records(*names)
+        names.each do |name|
+          scope "updated_#{name}_since".to_sym, lambda { |since|
+            {
+              :conditions => [updated_conditions_for_name(name), { :since_time => since }]
+            }
+          }
+        end
+        unless names.first == names.last
+          scope "updated_#{names.join('_or_')}_since".to_sym, lambda { |since|
+            {
+              :conditions => [names.collect { |name| updated_conditions_for_name(name) }.join(' or '), { :since_time => since }]
+            }
+          }
+          scope "updated_#{names.join('_and_')}_since".to_sym, lambda { |since|
+            {
+              :conditions => [names.collect { |name| updated_conditions_for_name(name) }.join(' and '), { :since_time => since }]
+            }
+          }
+        end
+      end
+
+      
       protected
 
       def conditions_for_name(name)
         "((select count(*) from #{connection.quote_table_name(name)} where #{connection.quote_table_name(name)}.#{connection.quote_column_name reflections[name].primary_key_name} = #{quoted_table_name}.#{connection.quote_column_name primary_key} and #{connection.quote_table_name(name)}.#{connection.quote_column_name("created_at")} > :since_time) > 0)"
       end
 
+      def updated_conditions_for_name(name)
+        "((select count(*) from #{connection.quote_table_name(name)} where #{connection.quote_table_name(name)}.#{connection.quote_column_name reflections[name].primary_key_name} = #{quoted_table_name}.#{connection.quote_column_name primary_key} and #{connection.quote_table_name(name)}.#{connection.quote_column_name("updated_at")} > :since_time) > 0)"
+      end
+      
     end
   end
 end
